bff6fd37160feb3cc9d975c2b2bc067a3c4dc6b5
Chansu Park <hhosu107@gmail.com>
2023-01-01 22:53:18 +0900

    Fix not working files

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -5,13 +5,13 @@ from . import models, schemas
 from pydantic import parse_obj_as
 
 def get_user(db: Session, user_id: int):
-    return schemas.User.from_orm(db.query(models.User).filter(models.User.id == user_id).first())
+    return db.query(models.User).filter(models.User.id == user_id).first()
 
 def get_user_by_email(db:Session, email: str):
-    return schemas.User.from_orm(db.query(models.User).filter(models.User.email == email).first())
+    return db.query(models.User).filter(models.User.email == email).first()
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return parse_obj_as(schemas.List[schemas.User], db.query(models.User).offset(skip).limit(limit).all())
+    return db.query(models.User).offset(skip).limit(limit).all()
 
 def create_user(db: Session, user: schemas.UserCreate):
     fake_hashes_password = user.password + "notreallyhashed"
@@ -19,15 +19,15 @@ def create_user(db: Session, user: schemas.UserCreate):
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
-    return schemas.User.from_orm(db_user)
+    return db_user
 
 def get_items(db: Session, skip: int = 0, limit: int = 100):
-    return parse_obj_as(schemas.List[schemas.Item], db.query(models.Item).offset(skip).limit(limit).all())
+    return db.query(models.Item).offset(skip).limit(limit).all()
 
 def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
     db_item = models.Item(**item.dict(), owner_id=user_id)
     db.add(db_item)
     db.commit()
     db.refresh(db_item)
-    return schemas.Item.from_orm(db_item)
+    return db_item
 
