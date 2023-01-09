d00715ebb871f33a94c1464aabbb2dcade017227
Chansu Park <hhosu107@gmail.com>
2023-01-09 23:05:30 +0900

    Fix

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -4,20 +4,35 @@ from . import models, schemas
 
 from pydantic import parse_obj_as
 
+# id is primary key
 def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    if db_user is None:
+        return None
+    return db_user
+    # return db.query(models.User).filter(models.User.id == user_id).first()
 
+# email is unique key
 def get_user_by_email(db:Session, email: str):
-    return db.query(models.User).filter(models.User.email == email).first()
+    db_user = db.query(models.User).filter(models.User.email == email).first()
+    if db_user is None:
+        return None
+    return schemas.User.from_orm(db_user)
+    # return db.query(models.User).filter(models.User.email == email).first()
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
 def create_user(db: Session, user: schemas.UserCreate):
+    # TODO: do hashing.
     fake_hashes_password = user.password + "notreallyhashed"
+    # Generates SQLAlchemy model instance
     db_user = models.User(email=user.email, hashed_password=fake_hashes_password)
+    # Add instance to DB
     db.add(db_user)
+    # Commit (after finishes, irreversible)
     db.commit()
+    # New data immediately appears when fetch.
     db.refresh(db_user)
     return db_user
 
--- sql_app/main.py
+++ sql_app/main.py
@@ -6,17 +6,19 @@ from sqlalchemy.orm import Session
 from . import crud, models, schemas
 from .database import SessionLocal, engine
 
+# Generates databse
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 # Dependency
 def get_db():
+    # For each request, generate new SessionLocal()
     db = SessionLocal()
     try:
-        yield db
+        yield db # waits request ends
     finally:
-        db.close()
+        db.close() # closes session local
 
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
